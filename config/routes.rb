37d74fbe8ed713f7aa67f6ea5fa9f46d8c3dd014
Tom Eloe <tomeloe@gmail.com>
2018-07-21 13:02:18 -0700

    added devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  root    'home#index'
-  get     'home/about'
-  get     'home/lookup'
-  post    'home/lookup' => 'home/lookup'
+  devise_for :users
+  root       'home#index'
+  get        'home/about'
+  get        'home/lookup'
+  post       'home/lookup' => 'home/lookup'
 end
