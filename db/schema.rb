36f6cb8fc6654f9d8b7571d81ee989f0bd723d5e
Tom Eloe <tomeloe@gmail.com>
2018-07-21 14:54:02 -0700

    created cryptos scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_21_195638) do
+ActiveRecord::Schema.define(version: 2018_07_21_215003) do
+
+  create_table "cryptos", force: :cascade do |t|
+    t.string "symbol"
+    t.integer "user_id"
+    t.decimal "cost_per"
+    t.decimal "amount_owned"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_cryptos_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
